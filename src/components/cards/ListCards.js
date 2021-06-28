8648b29543972852f55d2e93dda89266b35d469d
Maick1 <mtomalaz@unemi.edu.ec>
2021-06-28 01:50:08 -0500

    segunda parte

--- src/components/cards/ListCards.js
+++ src/components/cards/ListCards.js
@@ -1,20 +1,12 @@
-import Cards from "./Cards"
-import React, {useEffect, useState} from "react"
-import celebridades from '../../helpers/usuarios'
+import React from 'react'
+import Cards from './Cards'
 
 const ListCards = () => {
-    //console.log(celebridades)
     return (
-        <div className="container">
-        <div className="row">
-        { celebridades.map(user => (
-             <div className="col-md-4" key={user.login.uuid}>
-                <Cards user={user}/>
-             </div>
-        ))}
-            </div>
-                 </div>
-
+        <div>
+            <h1>Listado de Cartas</h1>
+            <Cards/>
+        </div>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
-import App from './App';
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-  <App/>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 
